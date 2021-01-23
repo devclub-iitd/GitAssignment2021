a6adae95a68682e0dfdb39856dbcb0d4b7ae9e83
Sreemanti Dey <sreemantidey1234@gmail.com>
2021-01-23 21:54:21 +0530

    Added NewEntry

--- src/Freshers.js
+++ src/Freshers.js
@@ -52,5 +52,12 @@ export const FRESHERS = [
         image: '/assets/cardImg.png',
         branch: "me2",
         about: 'Big fan of movies. Reading is not my cup of tea',
+    },
+    {
+        entryNum: 0393,
+        name : "Sreemanti Dey",
+        image: 'https://images.indianexpress.com/2020/01/Sreemanti-759.jpg',
+        branch: "CS1",
+        about: 'Love reading and writing. Love badminton. Wish to learn competitive coding, webdev and swimming',
     }
 ];
