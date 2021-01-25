fdaa4815da49ca342063f5675cb9f9f56cddab0b
sachit393 <sachitsachdeva27@hotmail.com>
2021-01-25 06:36:12 +0530

    hi

--- src/Freshers.js
+++ src/Freshers.js
@@ -53,4 +53,11 @@ export const FRESHERS = [
         branch: "me2",
         about: 'Big fan of movies. Reading is not my cup of tea',
     },
+    {
+        entryNum: 840,
+        name : "Sachit Sachdeva",
+        image: 'https://www.bing.com/images/search?view=detailV2&ccid=4%2bswZSVM&id=5D21EDF0A7553EE4B3B78C5EA1FB99F2D58C4DB6&thid=OIP.4-swZSVMu51RK50IyAjC1QHaEK&mediaurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fRe3eb3065254cbb9d512b9d08c808c2d5%3frik%3dtk2M1fKZ%252b6FejA%26riu%3dhttp%253a%252f%252fwww.pixelstalk.net%252fwp-content%252fuploads%252f2016%252f10%252fDesktop-nature-photo-hd-download-free.jpg%26ehk%3d5%252b5QNpcF%252blOtAonShvE%252froQCCQzfUdi7WbxIockMBzY%253d%26risl%3d%26pid%3dImgRaw&exph=1080&expw=1920&q=photo&simid=608030484809713154&ck=1988A282876F9AF9D7CB988892C9408D&selectedIndex=0&FORM=IRPRST&ajaxhist=0',
+        branch: "MT1",
+        about: 'nothing much',
+    },
 ];
