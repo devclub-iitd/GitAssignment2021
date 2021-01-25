18fc105ee60bc18fab88ae04540952aac383c802
Divyansh Agarrwal <divyanshagarrwal@gmail.com>
2021-01-25 16:26:54 +0530

    Commit2

--- src/Freshers.js
+++ src/Freshers.js
@@ -52,8 +52,7 @@ export const FRESHERS = [
         image: '/assets/cardImg.png',
         branch: "me2",
         about: 'Big fan of movies. Reading is not my cup of tea',
-    }
-
+    },
     {
         entryNum: 0488,
         name : "Divyansh Agarrwal",
