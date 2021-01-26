83f983e511647f3b8f7702dc0e353c3dee4ec54a
Kartik-57 <mt1200812@maths.iitd.ac.in>
2021-01-26 14:05:17 +0530

    Profile Added

--- src/Freshers.js
+++ src/Freshers.js
@@ -226,4 +226,11 @@ export const FRESHERS = [
         branch: "CS1",
         about: 'Love reading and writing. Love badminton. Wish to learn competitive coding, webdev and swimming',
     }
+  {
+        entryNum: 812,
+        name : "Kartik Rastogi",
+        image: '/assets/cardImg.png',
+        branch: "MT1",
+        about: 'Nothing Interesting.',
+    }
 ];
