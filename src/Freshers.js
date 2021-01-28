37980e01e61579431c93b67c0adbbdd3b9f655c9
alphaaviral <alphaaviral@gmail.com>
2021-01-28 19:40:37 +0530

    added entry

--- src/Freshers.js
+++ src/Freshers.js
@@ -53,4 +53,25 @@ export const FRESHERS = [
         branch: "me2",
         about: 'Big fan of movies. Reading is not my cup of tea',
     }
+    {
+        entryNum: 687,
+        name : 'Aviral Gupta',
+        image: '/assets/ph1200687.jpeg',
+        branch: 'PH1',
+        about: 'As may be evident from the department I am in; I love Physics, especially mechanics (although APL is making me reconsider my interests). I have always wanted to learn to code, and I hope that this thirst of mine will be quenched in the near future.',
+        contact: [
+            {
+                id: 0,
+                type: "insta",
+                url: 'https://www.instagram.com/alphaaviral/'
+            },
+            {
+                id: 1,
+                type: 'fb',
+                url: 'https://www.facebook.com/aviral.gupta.9484941'
+            }
+        ]
+    },
+
+
 ];
