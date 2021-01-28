07c0d227f8b47978a06453ba300b43fc31b2e7b8
Prabhat Singh <exploitdev0@gmail.com>
2021-01-28 12:42:34 +0530

    Kill The DJ

--- src/Freshers.js
+++ src/Freshers.js
@@ -1102,14 +1102,7 @@ export const FRESHERS = [
     name : "Prabhat Kumar",
     image: 'https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iKIWgaiJUtss/v2/150x-1.jpg',
     branch: "TT1",
-    about: 'I Love CS but landed in textile :(',
-    contact: [
-        {
-            id: 0,
-            type: 'insta',
-            url: 'https://www.instagram.com/bizarre9304/'
-        }
-             ]
+    about: 'I Love CS but landed in textile :('
 },
 ];
 
